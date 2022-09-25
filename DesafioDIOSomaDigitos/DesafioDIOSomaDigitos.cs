eee95c7f54ed7bb007b451554a8f29e426f75daa
PedroCunha08 <josepedroecunha@gmail.com>
2022-09-25 03:33:29 -0300

    Atualização
    Atualização

---
+++ DesafioDIOSomaDigitos/DesafioDIOSomaDigitos.cs
@@ -0,0 +1,65 @@
+// DESAFIO
+// Dado um número inteiro ( n ) , retorne a diferença entre o produto de seus dígitos e a soma de seus dígitos.
+
+// Entrada
+// A entrada consiste em um número inteiro ( n );
+
+// SAÍDA
+// A saída consiste na multiplicação do produto dos dígitos ( n ), a soma de seus dígitos e a diferenção entre o produto e a soma, como no exemplo a baixo: 
+
+// EXEMPLO 1
+// Entrada	Saída
+//   234	   15
+
+//   Explicação: 
+
+// Produto de dígitos = 2 * 3 * 4 = 24
+
+// Soma dos dígitos = 2 + 3 + 4 = 9
+
+// Resultado = 24 - 9 = 15
+
+// EXEMPLO 2:
+
+// Entrada	Saída
+//   4421  21
+// Explicação: 
+
+// Produto de dígitos = 4 * 4 * 2 * 1 = 32
+
+// Soma dos dígitos = 4 + 4 + 2 + 1 = 11
+
+// Resultado = 32 - 11 = 21
+
+// Restrições:
+
+// 1 <= n <= 10^5
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C_Aula06_OperadoresAritimeticos
+{
+    public class DesafioDIOSomaDigitos
+    {
+      static void Main(string[] args){
+
+        int n = int.Parse(Console.ReadLine());
+        int p = 1, s = 0; // onde p = produto e s = soma
+        while (n > 0)
+            {
+             int l = n % 10; //É o ultimo digito
+// TODO: Crie as outras condições necessárias para a resolução do desafio:
+             p *= l;
+             s += l;
+             n /=  10 ;
+           }
+
+           Console.WriteLine(p-s);
+        }
+    }
+}
